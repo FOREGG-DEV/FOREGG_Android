0b858b9ff3dfbd448c7c6bd2240583685ee6f414
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2024-05-07 21:35:43 +0900

    fix-buildSrc 복구

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,34 @@
+object Versions {
+    const val GRADLE = "7.4.2"
+
+    /* [ Kotlin } */
+    const val KOTLIN_VERSION = "1.7.10"
+    const val K_STDLIB = "1.7.10"
+    const val K_COROUTINES_CORE = "1.6.4"
+    const val K_COROUTINES = "1.3.9"
+
+    /* [ Google ] */
+    const val MATERIAL = "1.11.0"
+    const val HILT = "2.44"
+    const val GLIDE = "4.12.0"
+
+    /* [ Android ] */
+    const val CORE = "1.12.0"
+    const val APPCOMPAT = "1.6.1"
+    const val LIFECYCLE_KTX = "2.5.1"
+    const val VIEW_MODEL_KTX = "2.5.1"
+    const val CONSTRAINT_LAYOUT = "2.1.4"
+    const val FRAGMENT_KTX = "1.5.2"
+    const val NAVIGATION = "2.5.1"
+    const val REFRESH = "1.1.0"
+    const val DATA_STORE = "1.0.0"
+
+    /* [ Libraries ] */
+    const val RETROFIT = "2.9.0"
+    const val OKHTTP = "4.10.0"
+    const val VIEW_PAGER_INDICATOR = "5.0"
+    const val LOTTIE = "3.7.0"
+
+    /* [ Kakao ] */
+    const val KAKAO = "2.11.2"
+}
