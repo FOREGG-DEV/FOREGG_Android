96b790ffa6cb871becec85b60ac74c107e8e3e37
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2024-05-17 13:00:57 +0900

    ✨feat - 마이페이지 남편 로직

--- presentation/src/main/java/com/foregg/presentation/ui/main/profile/ProfileFragment.kt
+++ presentation/src/main/java/com/foregg/presentation/ui/main/profile/ProfileFragment.kt
@@ -2,8 +2,11 @@ package com.foregg.presentation.ui.main.profile
 
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.foregg.domain.model.enums.GenderType
+import com.foregg.presentation.R
 import com.foregg.presentation.base.BaseFragment
 import com.foregg.presentation.databinding.FragmentProfileBinding
+import com.foregg.presentation.util.UserInfo
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -27,6 +30,11 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfilePageState, P
 
         repeatOnStarted(viewLifecycleOwner) {
             launch {
+                viewModel.uiState.spouse.collect{
+                    if(UserInfo.info.genderType == GenderType.MALE) changeText(it)
+                }
+            }
+            launch {
                 viewModel.eventFlow.collect {
                     inspectEvent(it as ProfileEvent)
                 }
@@ -49,5 +57,11 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfilePageState, P
     private fun goToEdit(){
         val action = ProfileFragmentDirections.actionProfileToEdit()
         findNavController().navigate(action)
+    }
+
+    private fun changeText(spouse : String){
+        binding.apply {
+            btnMyMedicineInjection.text = getString(R.string.profile_spouse_medicine_injection, spouse)
+        }
     }
 }
