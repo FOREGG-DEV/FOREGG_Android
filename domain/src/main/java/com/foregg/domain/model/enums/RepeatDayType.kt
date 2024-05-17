eedc1ae13e11d50401afb230e12fa4897fd2f899
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2024-05-17 12:45:25 +0900

    ✨feat - 캘린더 생성, 수정 남편 로직

--- domain/src/main/java/com/foregg/domain/model/enums/RepeatDayType.kt
+++ domain/src/main/java/com/foregg/domain/model/enums/RepeatDayType.kt
@@ -3,4 +3,12 @@ package com.foregg.domain.model.enums
 enum class RepeatDayType(val week : String) {
     EVERY("매일"), MON("월"), TUE("화"), WED("수"),
     THU("목"), FRI("금"), SAT("토"), SUN("일");
+
+    companion object {
+        fun valuesOf(value: String): RepeatDayType {
+            return RepeatDayType.values().find {
+                it.week == value
+            } ?: RepeatDayType.MON
+        }
+    }
 }
