71cb5efd8da2fc7de6b44896aeb6e6283d91a68a
dogmania <hj296914@gmail.com>
2024-05-18 21:29:16 +0900

    :sparkles: Feat: 챌린지 동작에 대한 UseCase 구현

---
+++ domain/src/main/java/com/foregg/domain/usecase/home/challenge/ParticipateChallengeUseCase.kt
@@ -0,0 +1,15 @@
+package com.foregg.domain.usecase.home.challenge
+
+import com.foregg.domain.base.ApiState
+import com.foregg.domain.base.UseCase
+import com.foregg.domain.repository.ChallengeRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ParticipateChallengeUseCase @Inject constructor(
+    private val challengeRepository: ChallengeRepository
+) : UseCase<Long, ApiState<Unit>>() {
+    override suspend fun invoke(request: Long): Flow<ApiState<Unit>> {
+        return challengeRepository.participateChallenge(request)
+    }
+}
