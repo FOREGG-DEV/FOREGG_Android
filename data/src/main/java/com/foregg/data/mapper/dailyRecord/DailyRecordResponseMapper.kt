4cc1ae121a53ea54079f272a7f59080688f5f2ca
dogmania <hj296914@gmail.com>
2024-05-19 18:36:24 +0900

    :sparkles: Feat: dailyRecordMapper 구현

---
+++ data/src/main/java/com/foregg/data/mapper/dailyRecord/DailyRecordResponseMapper.kt
@@ -0,0 +1,24 @@
+package com.foregg.data.mapper.dailyRecord
+
+import com.foregg.data.base.Mapper
+import com.foregg.data.dto.dailyRecord.DailyRecordResponse
+import com.foregg.domain.model.response.DailyRecordResponseVo
+import com.foregg.domain.model.vo.DailyRecordResponseItemVo
+
+object DailyRecordResponseMapper: Mapper.ResponseMapper<DailyRecordResponse, DailyRecordResponseVo> {
+    override fun mapDtoToModel(type: DailyRecordResponse?): DailyRecordResponseVo {
+        return type?.run {
+            DailyRecordResponseVo(
+                dailyResponseDto = type.dailyResponseDTO.map {
+                    DailyRecordResponseItemVo(
+                        id = it.id,
+                        date = it.date,
+                        content = it.content,
+                        dailyConditionType = it.dailyConditionType,
+                        emotionType = it.emotionType
+                    )
+                }
+            )
+        } ?: DailyRecordResponseVo(emptyList())
+    }
+}
