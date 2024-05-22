3687100527e3a9515ecf3c1447129faa6f6c1c76
dogmania <hj296914@gmail.com>
2024-05-22 12:18:36 +0900

    :sparkles: Feat: 하루기록 뷰모델 구현

--- presentation/src/main/java/com/foregg/presentation/ui/dailyRecord/DailyRecordViewModel.kt
+++ presentation/src/main/java/com/foregg/presentation/ui/dailyRecord/DailyRecordViewModel.kt
@@ -3,29 +3,34 @@ package com.foregg.presentation.ui.dailyRecord
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.foregg.data.dto.dailyRecord.DailyRecordResponseItem
+import com.foregg.domain.model.enums.DailyRecordTabType
 import com.foregg.domain.model.response.DailyRecordResponseVo
 import com.foregg.domain.model.vo.DailyRecordResponseItemVo
 import com.foregg.domain.usecase.dailyRecord.GetDailyRecordUseCase
 import com.foregg.domain.usecase.dailyRecord.PostDailyRecordUseCase
+import com.foregg.presentation.Event
 import com.foregg.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class DailyRecordViewModel @Inject constructor(
-    private val postDailyRecordUseCase: PostDailyRecordUseCase,
     private val getDailyRecordUseCase: GetDailyRecordUseCase,
 ) : BaseViewModel<DailyRecordPageState>() {
     private val dailyRecordListStateFlow: MutableStateFlow<List<DailyRecordResponseItemVo>> = MutableStateFlow( emptyList() )
+    private val dailyRecordTabTypeStateFlow: MutableStateFlow<DailyRecordTabType> = MutableStateFlow(DailyRecordTabType.ADVERSE_EFFECT)
 
     override val uiState: DailyRecordPageState = DailyRecordPageState (
-        dailyRecordList = dailyRecordListStateFlow.asStateFlow()
+        dailyRecordList = dailyRecordListStateFlow.asStateFlow(),
+        dailyRecordTabType = dailyRecordTabTypeStateFlow.asStateFlow()
     )
 
-    init {
-//        getDailyRecord()
+    fun setView() {
+        getDailyRecord()
     }
 
     private fun getDailyRecord() {
@@ -42,9 +47,14 @@ class DailyRecordViewModel @Inject constructor(
         }
     }
 
-    private fun createDailyRecord() {
+    fun updateTabType(tabType: DailyRecordTabType) {
         viewModelScope.launch {
-//            postDailyRecordUseCase.invoke()
+            dailyRecordTabTypeStateFlow.update { tabType }
+            getDailyRecord()
         }
+    }
+
+    fun goToRecord() {
+        emitEventFlow(DailyRecordEvent.GoToCreateDailyRecordEvent)
     }
 }
