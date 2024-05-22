c8432253c1a97306640e6757065f7a9e1b41ae12
dogmania <hj296914@gmail.com>
2024-05-22 12:17:45 +0900

    :sparkles: Feat: 하루기록 리스트 조회, 하루기록 생성 페이지 이동 로직 구현

--- presentation/src/main/java/com/foregg/presentation/ui/dailyRecord/DailyRecordFragment.kt
+++ presentation/src/main/java/com/foregg/presentation/ui/dailyRecord/DailyRecordFragment.kt
@@ -6,26 +6,73 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.foregg.domain.model.enums.DailyConditionType
+import com.foregg.domain.model.enums.DailyRecordTabType
+import com.foregg.domain.model.vo.DailyRecordResponseItemVo
 import com.foregg.presentation.R
+import com.foregg.presentation.base.BaseFragment
+import com.foregg.presentation.databinding.FragmentDailyRecordBinding
+import com.foregg.presentation.ui.dailyRecord.adapter.DailyRecordAdapter
+import com.foregg.presentation.util.ForeggLog
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-class DailyRecordFragment : Fragment() {
+@AndroidEntryPoint
+class DailyRecordFragment : BaseFragment<FragmentDailyRecordBinding, DailyRecordPageState, DailyRecordViewModel>(
+    FragmentDailyRecordBinding::inflate
+) {
+    private val dailyRecordAdapter = DailyRecordAdapter()
 
-    companion object {
-        fun newInstance() = DailyRecordFragment()
+    override val viewModel: DailyRecordViewModel by viewModels()
+    override fun initView() {
+        binding.apply {
+            vm = viewModel
+            recordRecyclerView.adapter = dailyRecordAdapter
+            recordRecyclerView.layoutManager = LinearLayoutManager(context)
+        }
+        bindTab()
     }
 
-    private val viewModel: DailyRecordViewModel by viewModels()
+    override fun initStates() {
+        super.initStates()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        repeatOnStarted(viewLifecycleOwner) {
+            launch {
+                viewModel.uiState.dailyRecordList.collect {
+                    dailyRecordAdapter.submitList(it)
+                }
+            }
 
-        // TODO: Use the ViewModel
+            launch {
+                viewModel.eventFlow.collect {
+                    sortEvent(it as DailyRecordEvent)
+                }
+            }
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_daily_record, container, false)
+    private fun bindTab() {
+        binding.apply {
+            customTabBar.leftTab.setOnClickListener {
+                customTabBar.leftBtnClicked { viewModel.updateTabType(DailyRecordTabType.ADVERSE_EFFECT) }
+            }
+            customTabBar.rightTab.setOnClickListener {
+                customTabBar.rightBtnClicked { viewModel.updateTabType(DailyRecordTabType.DAILY_RECORD) }
+                binding.recordRecyclerView.adapter = dailyRecordAdapter
+            }
+        }
+    }
+
+    private fun sortEvent(event: DailyRecordEvent) {
+        when(event) {
+            DailyRecordEvent.GoToCreateDailyRecordEvent -> goToCreateDailyRecord()
+        }
+    }
+
+    private fun goToCreateDailyRecord() {
+        val action = DailyRecordFragmentDirections.actionDailyRecordToCreateDailyRecord()
+        findNavController().navigate(action)
     }
 }
