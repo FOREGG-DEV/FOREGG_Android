3ff0934057cd4dd410145db753f0064b78a50e0f
dogmania <hj296914@gmail.com>
2024-05-22 12:18:18 +0900

    :sparkles: Feat: 하루기록 상태 구현

--- presentation/src/main/java/com/foregg/presentation/ui/dailyRecord/DailyRecordPageState.kt
+++ presentation/src/main/java/com/foregg/presentation/ui/dailyRecord/DailyRecordPageState.kt
@@ -1,10 +1,12 @@
 package com.foregg.presentation.ui.dailyRecord
 
 import com.foregg.data.dto.dailyRecord.DailyRecordResponseItem
+import com.foregg.domain.model.enums.DailyRecordTabType
 import com.foregg.domain.model.vo.DailyRecordResponseItemVo
 import com.foregg.presentation.PageState
 import kotlinx.coroutines.flow.StateFlow
 
 data class DailyRecordPageState (
-    val dailyRecordList: StateFlow<List<DailyRecordResponseItemVo>>
+    val dailyRecordList: StateFlow<List<DailyRecordResponseItemVo>>,
+    val dailyRecordTabType: StateFlow<DailyRecordTabType>
 ): PageState
