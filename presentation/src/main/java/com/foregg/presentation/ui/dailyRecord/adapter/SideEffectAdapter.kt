906c2ef75e4bc183124bc8a1608d8326ca06821c
dogmania <hj296914@gmail.com>
2024-05-25 02:58:23 +0900

    :bento: Chore: 부작용 조회 어댑터, 뷰홀더 파일 생성

---
+++ presentation/src/main/java/com/foregg/presentation/ui/dailyRecord/adapter/SideEffectAdapter.kt
@@ -0,0 +1,4 @@
+package com.foregg.presentation.ui.dailyRecord.adapter
+
+class SideEffectAdapter {
+}
---
+++ presentation/src/main/java/com/foregg/presentation/ui/dailyRecord/adapter/SideEffectViewHolder.kt
@@ -0,0 +1,12 @@
+package com.foregg.presentation.ui.dailyRecord.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.foregg.presentation.databinding.ItemSideEffectBinding
+
+class SideEffectViewHolder(
+    private val binding: ItemSideEffectBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind() {
+
+    }
+}
