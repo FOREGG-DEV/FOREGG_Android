23dbca3c69bcca114ec1ae895fcebcf22154631d
dogmania <hj296914@gmail.com>
2024-05-26 01:02:00 +0900

    :recycle: Refactor: 챌린지 완료 다이얼로그 추가, 챌린지 리스트 state 변수 추가, 챌린지 조회, 챌린지 완료 api 함수 구현

--- presentation/src/main/java/com/foregg/presentation/ui/main/home/HomeViewModel.kt
+++ presentation/src/main/java/com/foregg/presentation/ui/main/home/HomeViewModel.kt
@@ -4,7 +4,10 @@ import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.foregg.domain.model.response.HomeRecordResponseVo
 import com.foregg.domain.model.response.HomeResponseVo
+import com.foregg.domain.model.response.MyChallengeListItemVo
 import com.foregg.domain.usecase.home.GetHomeUseCase
+import com.foregg.domain.usecase.home.challenge.CompleteChallengeUseCase
+import com.foregg.domain.usecase.home.challenge.GetMyChallengeUseCase
 import com.foregg.presentation.R
 import com.foregg.presentation.base.BaseViewModel
 import com.foregg.presentation.util.ForeggLog
@@ -13,6 +16,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.time.LocalDate
@@ -21,6 +25,8 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val getHomeUseCase: GetHomeUseCase,
+    private val getMyChallengeUseCase: GetMyChallengeUseCase,
+    private val completeChallengeUseCase: CompleteChallengeUseCase,
     private val resourceProvider: ResourceProvider
 ) : BaseViewModel<HomePageState>() {
     private val hasDailyRecordStateFlow : MutableStateFlow<Boolean> = MutableStateFlow(false)
@@ -28,6 +34,7 @@ class HomeViewModel @Inject constructor(
     private val todayDateStateFlow: MutableStateFlow<String> = MutableStateFlow("")
     private val todayScheduleStateFlow: MutableStateFlow<List<HomeRecordResponseVo>> = MutableStateFlow(emptyList())
     private val formattedTextStateFlow: MutableStateFlow<String> = MutableStateFlow("")
+    private val challengeListStateFlow: MutableStateFlow<List<MyChallengeListItemVo>> = MutableStateFlow(emptyList())
     val month = org.threeten.bp.LocalDate.now().monthValue
     val day = org.threeten.bp.LocalDate.now().dayOfMonth
 
@@ -36,13 +43,27 @@ class HomeViewModel @Inject constructor(
         userName = userNameStateFlow.asStateFlow(),
         todayDate = todayDateStateFlow.asStateFlow(),
         todayScheduleList = todayScheduleStateFlow.asStateFlow(),
-        formattedText = formattedTextStateFlow.asStateFlow()
+        formattedText = formattedTextStateFlow.asStateFlow(),
+        challengeList = challengeListStateFlow.asStateFlow()
     )
 
     fun initScheduleStates() {
+        getTodaySchedule()
+        getMyChallenge()
+    }
+
+    private fun getTodaySchedule() {
         viewModelScope.launch() {
             getHomeUseCase(Unit).collect {
                 resultResponse(it, ::handleInitScheduleStatesSuccess, { ForeggLog.D("실패") })
+            }
+        }
+    }
+
+    private fun getMyChallenge() {
+        viewModelScope.launch {
+            getMyChallengeUseCase(Unit).collect { it ->
+                resultResponse(it, { updateChallengeList(it) })
             }
         }
     }
@@ -53,6 +74,12 @@ class HomeViewModel @Inject constructor(
             todayDateStateFlow.update { result.todayDate }
             todayScheduleStateFlow.update { splitTodayScheduleByRepeatedTime(result.homeRecordResponseVo) }
             formattedTextStateFlow.update { resourceProvider.getString(R.string.today_schedule_format, userNameStateFlow.value, month, day) }
+        }
+    }
+
+    private fun updateChallengeList(newList: List<MyChallengeListItemVo>) {
+        viewModelScope.launch {
+            challengeListStateFlow.update { newList }
         }
     }
 
@@ -68,6 +95,14 @@ class HomeViewModel @Inject constructor(
     private fun splitListItem(list: HomeRecordResponseVo): List<HomeRecordResponseVo> {
         return list.times.map { repeatTime ->
             list.copy(times = listOf(repeatTime))
+        }
+    }
+
+    fun completeChallenge(id: Long) {
+        viewModelScope.launch {
+            completeChallengeUseCase(id).collect {
+                resultResponse(it, { getMyChallenge() })
+            }
         }
     }
 
