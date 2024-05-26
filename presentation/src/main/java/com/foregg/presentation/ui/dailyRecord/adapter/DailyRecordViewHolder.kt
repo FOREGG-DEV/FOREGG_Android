b8040eb9abc3a5365a7ecb0d983a17973b974fb3
dogmania <hj296914@gmail.com>
2024-05-26 13:39:46 +0900

    :recycle: Refactor: 함수 분리

--- presentation/src/main/java/com/foregg/presentation/ui/dailyRecord/adapter/DailyRecordViewHolder.kt
+++ presentation/src/main/java/com/foregg/presentation/ui/dailyRecord/adapter/DailyRecordViewHolder.kt
@@ -14,27 +14,39 @@ class DailyRecordViewHolder(
 ) : RecyclerView.ViewHolder(binding.root){
     fun bind(item: DailyRecordResponseItemVo) {
         binding.apply {
-            when(item.dailyConditionType) {
-                DailyConditionType.WORST -> imageViewEmotion.setImageResource(R.drawable.ic_emotion_worst_selected)
-                DailyConditionType.BAD -> imageViewEmotion.setImageResource(R.drawable.ic_emotion_bad_selected)
-                DailyConditionType.SOSO -> imageViewEmotion.setImageResource(R.drawable.ic_emotion_soso_selected)
-                DailyConditionType.GOOD -> imageViewEmotion.setImageResource(R.drawable.ic_emotion_smile_selected)
-                DailyConditionType.PERFECT -> imageViewEmotion.setImageResource(R.drawable.ic_emotion_perfect_selected)
-                DailyConditionType.DEFAULT -> imageViewEmotion.visibility = View.GONE
-            }
-            when(item.emotionType) {
-                EmotionType.HEART -> imageViewEmotionHusband.setImageResource(R.drawable.ic_heart)
-                EmotionType.THUMBS_UP -> imageViewEmotionHusband.setImageResource(R.drawable.ic_thumbs_up)
-                EmotionType.CLAP -> imageViewEmotionHusband.setImageResource(R.drawable.ic_clap)
-                EmotionType.SAD -> imageViewEmotionHusband.setImageResource(R.drawable.ic_emotion_sad_selected)
-                EmotionType.SMILE -> imageViewEmotionHusband.setImageResource(R.drawable.ic_emotion_perfect_selected)
-                EmotionType.DEFAULT -> imageViewEmotionHusband.visibility = View.GONE
-            }
-            if (TimeFormatter.getToday() == item.date) {
-                imageViewLine.setImageResource(R.drawable.ic_today_record_item_line)
-            } else imageViewLine.setImageResource(R.drawable.ic_record_item_line)
+            val imageResource = setImageResource(item.dailyConditionType)
+            val husbandImageResource = setImageResource(item.emotionType)
+
+            imageViewEmotion.setImageResource(imageResource)
+            imageViewEmotionHusband.setImageResource(husbandImageResource)
+
+            if (TimeFormatter.getToday() == item.date) { imageViewLine.setImageResource(R.drawable.ic_today_record_item_line) }
+            else imageViewLine.setImageResource(R.drawable.ic_record_item_line)
+
             textDate.text = item.date
             dailyRecordText.text = item.content
+        }
+    }
+
+    private fun setImageResource(type: DailyConditionType): Int {
+        return when(type) {
+            DailyConditionType.WORST -> R.drawable.ic_emotion_worst_selected
+            DailyConditionType.BAD -> R.drawable.ic_emotion_bad_selected
+            DailyConditionType.SOSO -> R.drawable.ic_emotion_soso_selected
+            DailyConditionType.GOOD -> R.drawable.ic_emotion_smile_selected
+            DailyConditionType.PERFECT -> R.drawable.ic_emotion_perfect_selected
+            DailyConditionType.DEFAULT -> View.GONE
+        }
+    }
+
+    private fun setImageResource(type: EmotionType): Int {
+        return when(type) {
+            EmotionType.HEART -> R.drawable.ic_heart
+            EmotionType.THUMBS_UP -> R.drawable.ic_thumbs_up
+            EmotionType.CLAP -> R.drawable.ic_clap
+            EmotionType.SAD -> R.drawable.ic_emotion_sad_selected
+            EmotionType.SMILE -> R.drawable.ic_emotion_perfect_selected
+            EmotionType.DEFAULT -> View.GONE
         }
     }
 }
