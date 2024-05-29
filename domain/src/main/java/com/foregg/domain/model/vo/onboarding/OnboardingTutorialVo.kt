6c0cd9ce86497c9540519b7cf3c90e17e6c5b47a
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2024-05-29 13:51:28 +0900

    ✨feat - 온보딩 이전, 스킵, 내용 추가 (이미지 추가 아직)

---
+++ domain/src/main/java/com/foregg/domain/model/vo/onboarding/OnboardingTutorialVo.kt
@@ -0,0 +1,7 @@
+package com.foregg.domain.model.vo.onboarding
+
+data class OnboardingTutorialVo(
+    val title : String,
+    val content : String,
+    val img : Int
+)
--- presentation/src/main/java/com/foregg/presentation/ui/sign/onBoarding/adapter/OnboardingTutorialViewHolder.kt
+++ presentation/src/main/java/com/foregg/presentation/ui/sign/onBoarding/adapter/OnboardingTutorialViewHolder.kt
@@ -1,15 +1,17 @@
 package com.foregg.presentation.ui.sign.onBoarding.adapter
 
 import androidx.recyclerview.widget.RecyclerView
+import com.foregg.domain.model.vo.onboarding.OnboardingTutorialVo
 import com.foregg.presentation.databinding.IncludeItemTutorialBinding
 
 class OnboardingTutorialViewHolder(
     private val binding: IncludeItemTutorialBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(item : String) {
+    fun bind(item : OnboardingTutorialVo) {
         binding.apply {
-            textContent.text = item
+            textTitle.text = item.title
+            textContent.text = item.content
         }
     }
 }
