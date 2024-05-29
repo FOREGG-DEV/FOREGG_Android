af9f57e6a9dd3aaa6a1afc69e4dac34e33f68e47
dogmania <hj296914@gmail.com>
2024-05-29 15:55:17 +0900

    :sparkles: Feat: 정보 아이템 Vo 구현

---
+++ domain/src/main/java/com/foregg/domain/model/vo/InfoItemVo.kt
@@ -0,0 +1,6 @@
+package com.foregg.domain.model.vo
+
+data class InfoItemVo(
+    val title: String = "",
+    val image: String = "http://www.iconkr.com/files/attach/images/9319/753/021/a2f39d72c7227ded2e524c2b6585961d.jpg"
+)
