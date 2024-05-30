f613c2fdc71b66aa8e8fd7e287f17708239c3665
dogmania <hj296914@gmail.com>
2024-05-30 22:16:24 +0900

    :sparkles: Feat: 홈 화면 챌린지 목록에서 오늘 챌린지 성공한 경우 버튼 디자인 변경 로직 구현

--- presentation/src/main/java/com/foregg/presentation/ui/main/home/adapter/HomeChallengeViewHolder.kt
+++ presentation/src/main/java/com/foregg/presentation/ui/main/home/adapter/HomeChallengeViewHolder.kt
@@ -2,13 +2,16 @@ package com.foregg.presentation.ui.main.home.adapter
 
 import androidx.recyclerview.widget.RecyclerView
 import com.foregg.domain.model.response.MyChallengeListItemVo
+import com.foregg.presentation.R
 import com.foregg.presentation.databinding.ItemHomeMyChallengeBinding
+import com.foregg.presentation.util.TimeFormatter
+import org.threeten.bp.LocalDate
 
 class HomeChallengeViewHolder(
     private val binding: ItemHomeMyChallengeBinding,
     private val listener: HomeChallengeAdapter.HomeChallengeDelegate
 ) : RecyclerView.ViewHolder(binding.root) {
-
+    private val today = TimeFormatter.getKoreanDayOfWeek(LocalDate.now().dayOfWeek)
     private var itemId: Long? = null
 
     init {
@@ -23,6 +26,14 @@ class HomeChallengeViewHolder(
         itemId = item.id
         binding.apply {
             textChallengeName.text = item.name
+            item.successDays?.let {list ->
+                for (i in list.indices) {
+                    if (list[i] == today) {
+                        btnCompleteChallenge.setImageResource(R.drawable.ic_btn_complete_challenge_already)
+                        btnCompleteChallenge.isClickable = false
+                    }
+                }
+            }
         }
     }
 }
