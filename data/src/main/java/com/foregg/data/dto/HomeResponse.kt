17a1356dd041b701c8dba00f53e070dd04132859
dogmania <hj296914@gmail.com>
2024-05-31 04:58:02 +0900

    :recycle: Refacator: Reponse 데이터 클래스 프로퍼티 수정

--- data/src/main/java/com/foregg/data/dto/HomeResponse.kt
+++ data/src/main/java/com/foregg/data/dto/HomeResponse.kt
@@ -1,14 +1,25 @@
 package com.foregg.data.dto
 
 import com.foregg.data.base.DataDto
+import com.foregg.domain.model.enums.DailyConditionType
 import com.foregg.domain.model.response.HomeRecordResponseVo
 import com.google.gson.annotations.SerializedName
 
 data class HomeResponse(
     @SerializedName("userName")
-    val userName: String,
+    val userName: String = "",
+    @SerializedName("spouseName")
+    val spouseName: String = "",
     @SerializedName("todayDate")
-    val todayDate: String,
+    val todayDate: String = "",
     @SerializedName("homeRecordResponseDTO")
-    val homeRecordResponseVo: List<HomeRecordResponseVo>
+    val homeRecordResponseVo: List<HomeRecordResponseVo> = emptyList(),
+    @SerializedName("ssn")
+    val ssn: String = "",
+    @SerializedName("dailyConditionType")
+    val dailyConditionType: DailyConditionType = DailyConditionType.DEFAULT,
+    @SerializedName("dailyContent")
+    val dailyContent: String = "",
+    @SerializedName("latestMedicalRecord")
+    val latestMedicalRecord: String = ""
 ) : DataDto
