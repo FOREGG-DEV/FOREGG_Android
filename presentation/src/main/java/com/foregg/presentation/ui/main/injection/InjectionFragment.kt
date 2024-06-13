22bbfc2c74922ea8a41c21f869c88486442c8b50
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2024-06-13 17:54:22 +0900

    feat - 카카오 공유하기 기능 추가 (버튼 안됨)

--- presentation/src/main/java/com/foregg/presentation/ui/main/injection/InjectionFragment.kt
+++ presentation/src/main/java/com/foregg/presentation/ui/main/injection/InjectionFragment.kt
@@ -1,5 +1,6 @@
 package com.foregg.presentation.ui.main.injection
 
+import android.content.ActivityNotFoundException
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -8,10 +9,18 @@ import com.foregg.domain.model.enums.GenderType
 import com.foregg.presentation.R
 import com.foregg.presentation.base.BaseFragment
 import com.foregg.presentation.databinding.FragmentInjectionBinding
+import com.foregg.presentation.util.ForeggLog
 import com.foregg.presentation.util.ForeggToast
 import com.foregg.presentation.util.PendingExtraValue
 import com.foregg.presentation.util.SvgImageUtil
 import com.foregg.presentation.util.UserInfo
+import com.kakao.sdk.common.util.KakaoCustomTabsClient
+import com.kakao.sdk.share.ShareClient
+import com.kakao.sdk.share.WebSharerClient
+import com.kakao.sdk.template.model.Button
+import com.kakao.sdk.template.model.Content
+import com.kakao.sdk.template.model.FeedTemplate
+import com.kakao.sdk.template.model.Link
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -52,12 +61,60 @@ class InjectionFragment : BaseFragment<FragmentInjectionBinding, InjectionPageSt
     private fun inspectEvent(event: InjectionEvent){
         when(event){
             InjectionEvent.GoToHomeEvent -> findNavController().popBackStack()
-            InjectionEvent.ErrorShareToast -> ForeggToast.createToast(requireContext(), R.string.toast_error_no_exist_spouse, Toast.LENGTH_SHORT).show()
+            InjectionEvent.ErrorShareToast -> shareWithKakao()
             InjectionEvent.SuccessShareToast -> ForeggToast.createToast(requireContext(), R.string.toast_success_share_injection, Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun loadImage(url : String){
         SvgImageUtil.loadImageFromUrl(binding.imgInjection, url, requireContext(), loadingDrawable = R.drawable.img_injection_loading, errorDrawable = R.drawable.img_injection_loading)
+    }
+
+    private fun shareWithKakao(){
+        val feed = getFeed()
+        if (ShareClient.instance.isKakaoTalkSharingAvailable(requireContext())) {
+            ShareClient.instance.shareDefault(requireContext(), feed) { sharingResult, error ->
+                if (error != null) {
+                    ForeggLog.D("카카오톡 공유 실패")
+                }
+                else if (sharingResult != null) {
+                    startActivity(sharingResult.intent)
+                }
+            }
+        } else {
+            val sharerUrl = WebSharerClient.instance.makeDefaultUrl(feed)
+            try {
+                KakaoCustomTabsClient.openWithDefault(requireContext(), sharerUrl)
+            } catch(e: UnsupportedOperationException) {
+                ForeggLog.D("CustomTabsServiceConnection 지원 브라우저가 없을 때 예외처리")
+            }
+            try {
+                KakaoCustomTabsClient.open(requireContext(), sharerUrl)
+            } catch (e: ActivityNotFoundException) {
+                ForeggLog.D("디바이스에 설치된 인터넷 브라우저가 없을 때 예외처리")
+            }
+        }
+    }
+
+    private fun getFeed() : FeedTemplate{
+        return FeedTemplate(
+            content = Content(
+                title = getString(R.string.kakao_share_title),
+                description = getString(R.string.kakao_share_content),
+                imageUrl = "https://drive.google.com/uc?export=download&id=1gOKA4dUyoZPayh5re4Gx9gISK0PyJUTj",
+                link = Link(
+                    mobileWebUrl = getString(R.string.kakao_share_url)
+                )
+            ),
+            buttons = listOf(
+                Button(
+                    getString(R.string.kakao_share_button),
+                    Link(
+                        webUrl = getString(R.string.kakao_share_url),
+                        mobileWebUrl = getString(R.string.kakao_share_url),
+                    )
+                )
+            )
+        )
     }
 }
