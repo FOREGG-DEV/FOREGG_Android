51b73c987784bf33ec48728ed8833d6e4b707874
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2024-07-14 22:21:33 +0900

    version up

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.foregg.presentation"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.1.1"
+        versionCode = 6
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
