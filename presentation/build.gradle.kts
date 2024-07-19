3298594242480d1e5aa5ab3f5a880c5eb37221b3
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2024-07-19 22:45:30 +0900

    버전 업

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.foregg.presentation"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.1.6"
+        versionCode = 11
+        versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
