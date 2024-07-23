3abe767fa42135678edc126a80486181b583b0c1
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2024-07-23 12:41:38 +0900

    fix - 가계부 삭제 버튼 수정

--- presentation/src/main/java/com/foregg/presentation/ui/main/account/AccountFragment.kt
+++ presentation/src/main/java/com/foregg/presentation/ui/main/account/AccountFragment.kt
@@ -35,7 +35,7 @@ class AccountFragment : BaseFragment<FragmentAccountBinding, AccountPageState, A
             }
 
             override fun changeMode(selectMode: Boolean) {
-                val res = if(selectMode) R.drawable.ic_delete_main else R.drawable.ic_btn_add_filled_main
+                val res = if(selectMode) R.drawable.ic_delete_delete else R.drawable.ic_btn_add_filled_main
                 binding.imgBtnAddAccount.setImageResource(res)
             }
         })
